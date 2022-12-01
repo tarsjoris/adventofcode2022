398a08371b96e665e4e5da533960e3796055e83a
tjoris <tjoris@tjoriss-MacBook-Pro.local>
2022-12-02 00:26:06 +0100

    day1part1

---
+++ day1part1/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() {
+    let calories_per_elve = read_input("input.txt");
+    let most = get_most_calories(calories_per_elve);
+    println!("Answer: {most}");
+}
+
+fn get_most_calories(calories_per_elve: Vec<Vec<i32>>) -> i32 {
+    return calories_per_elve.iter()
+        .map(|cals| cals.iter().sum())
+        .max()
+        .unwrap_or(0);
+}
+
+fn read_input(filename: &str) -> Vec<Vec<i32>> {
+    let lines = match read_lines(filename) {
+        Err(why) => panic!("couldn't open {filename}: {why}"),
+        Ok(lines) => lines,
+    };
+
+    let mut calories_per_elve = Vec::new();
+    let mut calories = Vec::new();
+    for line_result in lines {
+        let line = match line_result {
+            Err(why) => panic!("couldn't read line: {why}"),
+            Ok(line) => line,
+        };
+        if line == "" {
+            calories_per_elve.push(calories);
+            calories = Vec::new();
+        } else {
+            let val = match line.parse::<i32>() {
+                Err(why) => panic!("not a number ({line}): {why}"),
+                Ok(val) => val,
+            };
+            calories.push(val);
+        }
+    }
+    calories_per_elve.push(calories);
+    return calories_per_elve;
+}
+
+fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>> {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
