9c83997e196b044f913cdc20545f3afba9c4c246
samsung <samsung@samsungs-MacBook-Pro.local>
2022-12-22 17:21:51 +0100

    wip day16part2 tunnel length

--- day16part2/src/main.rs
+++ day16part2/src/main.rs
@@ -3,12 +3,88 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 
+type Tunnel = (String, i8);
+
 struct Valve {
+    name: String,
     rate: u32,
-    tunnels: Vec<String>,
+    tunnels: Vec<Tunnel>,
 }
 
-type Scan = HashMap<String, Valve>;
+impl Valve {
+    fn replace_tunnel(&mut self, valve_name_to_remove: &String, valve_to_remove: &Valve) {
+        if let Some(position) = self
+            .tunnels
+            .iter()
+            .position(|(name, _)| name == valve_name_to_remove)
+        {
+            let (_, removed_tunnel_length) = self.tunnels.remove(position);
+            for (tunnel_name, length) in valve_to_remove.tunnels.iter() {
+                if *tunnel_name != self.name {
+                    let new_tunnel_length = removed_tunnel_length + length;
+                    if let Some(position) = self
+                        .tunnels
+                        .iter()
+                        .position(|(name, _)| name == tunnel_name)
+                    {
+                        let (_, existing_tunnel_length) = self.tunnels.get(position).unwrap();
+                        if new_tunnel_length < *existing_tunnel_length {
+                            self.tunnels.remove(position);
+                            self.tunnels
+                                .push((tunnel_name.to_string(), new_tunnel_length));
+                        }
+                    } else {
+                        self.tunnels
+                            .push((tunnel_name.to_string(), new_tunnel_length));
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct Scan {
+    valves: HashMap<String, Valve>,
+}
+
+impl Scan {
+    fn get_valve_count(&self) -> usize {
+        self.valves.len()
+    }
+
+    fn get_useless_valve_names(&self) -> Vec<String> {
+        self.valves
+            .iter()
+            .filter(|(_, valve)| valve.rate == 0)
+            .map(|(name, _)| name.to_string())
+            .collect()
+    }
+
+    fn remove_useless_valves(&mut self) {
+        for useless_valve_name in self.get_useless_valve_names() {
+            if useless_valve_name != "AA" {
+                self.remove_valve(&useless_valve_name);
+            }
+        }
+    }
+
+    fn remove_valve(&mut self, valve_name_to_remove: &String) {
+        let valve_to_remove = self.valves.remove(valve_name_to_remove).unwrap();
+        for valve in self.valves.values_mut() {
+            valve.replace_tunnel(valve_name_to_remove, &valve_to_remove);
+        }
+    }
+
+    fn print(&self) {
+        self.valves.iter().for_each(|(valve_name, valve)| {
+            println!("{valve_name}");
+            valve
+                .tunnels
+                .iter()
+                .for_each(|tunnel| println!("  Tunnel to {} in {}", tunnel.0, tunnel.1));
+        });
+    }
+}
 
 enum ValveList<'a> {
     Last(Vec<&'a String>),
@@ -16,15 +92,13 @@ enum ValveList<'a> {
 }
 
 fn main() {
-    let scan = read_input("input.txt");
-    let useless_valve_names: Vec<&String> = scan
-        .iter()
-        .filter(|(_, valve)| valve.rate == 0)
-        .map(|(name, _)| name)
-        .collect();
-    let closed_valve_count = scan.len() - useless_valve_names.len();
-    let open_valves = ValveList::Last(useless_valve_names);
     let first_node = "AA".to_string();
+    let mut scan = read_input("input.txt");
+    scan.remove_useless_valves();
+    scan.print();
+    let useless_valve_names = scan.get_useless_valve_names();
+    let closed_valve_count = scan.get_valve_count() - useless_valve_names.len();
+    let open_valves = ValveList::Last(useless_valve_names.iter().collect());
     let trail_without_opening = ValveList::Last(vec![&first_node]);
     let player_state = PlayerState {
         minutes_left: 26,
@@ -42,7 +116,7 @@ fn main() {
 }
 
 struct PlayerState<'a> {
-    minutes_left: u8,
+    minutes_left: i8,
     current_valve_name: &'a String,
     trail_without_opening: &'a ValveList<'a>,
 }
@@ -84,26 +158,27 @@ fn get_most_pressure_release(
             },
             player_b,
         );
-        let extra_release = scan.get(player_a.current_valve_name).unwrap().rate
+        let extra_release = scan.valves.get(player_a.current_valve_name).unwrap().rate
             * (player_a.minutes_left as u32 - 1);
         most_pressure_release = child_pressure_release + extra_release;
     }
     let max_tunnels_pressure_release = scan
+        .valves
         .get(player_a.current_valve_name)
         .unwrap()
         .tunnels
         .iter()
-        .filter(|tunnel| !contains(player_a.trail_without_opening, tunnel))
-        .map(|tunnel| {
+        .filter(|tunnel| !contains(player_a.trail_without_opening, &tunnel.0))
+        .map(|(tunnel_name, tunnel_length)| {
             let new_trail_without_opening =
-                ValveList::Node(tunnel, &player_a.trail_without_opening);
+                ValveList::Node(tunnel_name, &player_a.trail_without_opening);
             get_most_pressure_release(
                 scan,
                 closed_valve_count,
                 open_valves,
                 &PlayerState {
-                    minutes_left: player_a.minutes_left - 1,
-                    current_valve_name: tunnel,
+                    minutes_left: player_a.minutes_left - tunnel_length,
+                    current_valve_name: tunnel_name,
                     trail_without_opening: &&new_trail_without_opening,
                 },
                 player_b,
@@ -133,13 +208,15 @@ fn read_input(filename: &str) -> Scan {
         Err(why) => panic!("couldn't open {filename}: {why}"),
         Ok(lines) => lines,
     };
-    lines
-        .map(|line| match line {
-            Err(why) => panic!("couldn't read line: {why}"),
-            Ok(l) => l,
-        })
-        .map(|line| parse_valve(&line))
-        .collect()
+    Scan {
+        valves: lines
+            .map(|line| match line {
+                Err(why) => panic!("couldn't read line: {why}"),
+                Ok(l) => l,
+            })
+            .map(|line| parse_valve(&line))
+            .collect(),
+    }
 }
 
 // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
@@ -158,9 +235,17 @@ fn parse_valve(line: &String) -> (String, Valve) {
     parts.next(); // valves/valve
     let tunnels = parts
         .filter(|s| !s.is_empty())
-        .map(|s| s.to_string())
+        .map(|s| (s.to_string(), 1))
         .collect();
-    (name, Valve { rate, tunnels })
+    let valve_name = name.to_string();
+    (
+        name,
+        Valve {
+            name: valve_name,
+            rate,
+            tunnels,
+        },
+    )
 }
 
 fn assert_part(part: Option<&str>, expected_literal: &str) {
